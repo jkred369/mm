72fb855272707f7a1a29f1da14bced8256f809fd
alexsuo <alex.suo@gmail.com>
2017-09-25 21:23:12 +0800

    Implement the hash dispatcher.

---
+++ lib/system/src/Dispatcher.hpp
@@ -0,0 +1,173 @@
+/*
+ * IDispatcher.hpp
+ *
+ *  Created on: Sep 24, 2017
+ *      Author: suoalex
+ */
+
+#ifndef LIB_SYSTEM_DISPATCHER_HPP_
+#define LIB_SYSTEM_DISPATCHER_HPP_
+
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+namespace mm
+{
+	typedef std::function<void()> Runnable;
+
+	//
+	// The basic execution unit of a hash dispatcher, this class is simply
+	// a thread taking all tasks from a given queue.
+	//
+	// The queue is expected to be thread-safe and the thread will execute tasks from the queue.
+	//
+	template<typename Queue> class TaskRunner
+	{
+	public:
+
+		//
+		// Constructor.
+		//
+		// waitOnEmpty : Flag if thread should enter passive wait when queue is empty.
+		//
+		TaskRunner(bool waitOnEmpty = true) : waitOnEmpty(waitOnEmpty), stopRequested(false), thread(nullptr)
+		{
+		}
+
+		//
+		// Destructor.
+		//
+		~TaskRunner()
+		{
+			if (thread != nullptr)
+			{
+				delete thread;
+			}
+		}
+
+		//
+		// Start execution of the tasks.
+		//
+		void start()
+		{
+			// sanity check
+			if (thread != nullptr)
+			{
+				return;
+			}
+
+			thread = new std::thread([] ()
+			{
+				std::shared_ptr<Runnable> runnable;
+
+				while (!stopRequested)
+				{
+					if (queue.pop(runnable))
+					{
+						runnable();
+					}
+					else if (waitOnEmpty)
+					{
+						condition.wait();
+					}
+				}
+			});
+		}
+
+		//
+		// Stop the runner and optionally wait for the tasks to finish.
+		//
+		// waitForDone : Flag if the thread should be stopped immediately.
+		//
+		void stop()
+		{
+			stopRequested = true;
+			thread->join();
+		}
+
+		//
+		// Submit a task into the runner.
+		//
+		// runnable : The task to be executed.
+		//
+		void submit(std::shared_ptr<Runnable>& runnable)
+		{
+			bool wasEmpty = waitOnEmpty ? queue.empty() : false;
+
+			queue.push_back(runnable);
+
+			// notify where needed - we know there is only 1 thread waiting at most.
+			if (wasEmpty && waitOnEmpty)
+			{
+				condition.notifyOne();
+			}
+		}
+
+	private:
+
+		// Flag if the thread should wait when queue is empty.
+		const bool waitOnEmpty;
+
+		// Flag if stop is called.
+		bool stopRequested;
+
+		// Pointer to actual thread.
+		std::thread* thread;
+
+		// Condition used for queue notify.
+		Condition condition;
+
+		// The queue structure holding the tasks.
+		Queue<std::shared_ptr<Runnable> > queue;
+
+	};
+
+	//
+	// A hash dispatcher.
+	//
+	// The tasks are separated into queues based on the hash value of the key submitted.
+	//
+	template<typename Key, typename Thread, typename Queue> class HashDispatcher
+	{
+	public:
+
+		// The hash function type.
+		typedef std::function<std::int32_t(const Key&) const> HashFunction;
+
+		//
+		// Constructor.
+		//
+		// threadCount : number of threads in the dispatcher.
+		//
+		HashDispatcher(const HashFunction& function, size_t threadCount = 4) :
+			function(function),
+			runners(threadCount)
+		{
+		}
+
+		//
+		// Submit a task for the dispatcher to run.
+		//
+		// key : The key for the task.
+		// runnable : The task to be executed.
+		//
+		void submit(Key key, std::shared_ptr<Runnable>& runnable)
+		{
+			runners[function(key) % runners.size()].submit(runnable);
+		}
+
+	private:
+
+		// The hash function.
+		const HashFunction& function;
+
+		// The task runners.
+		std::vector<TaskRunner> runners;
+	};
+}
+
+#endif /* LIB_SYSTEM_DISPATCHER_HPP_ */
