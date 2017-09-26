1d4aab8217fce0599c17ad485ce436c38fc09672
alexsuo <alex.suo@gmail.com>
2017-09-26 21:31:20 +0800

    Add test case.

---
+++ lib/common/test/ConditionTest.cpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <chrono>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include <Condition.hpp>
+
+namespace mm
+{
+	TEST(ConditionTest, DummyCase)
+	{
+		mm::Condition<> condition;
+		bool done = false;
+
+		std::thread t1([&condition, &done] ()
+		{
+			condition.wait();
+			done = true;
+		});
+
+		std::thread t2([condition] ()
+		{
+			condition.notifyOne();
+		});
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		ASSERT_TRUE(done);
+	}
+}
