444e3fd213373f3b8c7dab850aad25a62bb851c3
alexsuo <alex.suo@gmail.com>
2017-10-02 10:02:38 +0800

    Add more test cases.

--- lib/common/test/CountDownLatchTest.cpp
+++ lib/common/test/CountDownLatchTest.cpp
@@ -8,6 +8,23 @@
 
 namespace mm
 {
+	TEST(CountDownLatchTest, DummyCase)
+	{
+		mm::CountDownLatch<> latch(0);
+		bool done = false;
+
+		std::thread t1([&latch, &done] ()
+		{
+			latch.wait();
+			done = true;
+		});
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		ASSERT_TRUE(done);
+
+		t1.join();
+	}
+
 	TEST(CountDownLatchTest, ThreadOrderingCase)
 	{
 		mm::CountDownLatch<> latch(1);
@@ -21,14 +38,45 @@ namespace mm
 
 		std::thread t2([&latch] ()
 		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 			latch.countDown();
 		});
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
 		ASSERT_TRUE(done);
 
 		t1.join();
 		t2.join();
+	}
+
+	TEST(CountDownLatchTest, MultiThreadCase)
+	{
+		mm::CountDownLatch<> latch(2);
+		bool done = false;
+
+		std::thread t1([&latch, &done] ()
+		{
+			latch.wait();
+			done = true;
+		});
+
+		std::thread t2([&latch] ()
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			latch.countDown();
+		});
+
+		std::thread t3([&latch] ()
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			latch.countDown();
+		});
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(30));
+		ASSERT_TRUE(done);
+
+		t1.join();
+		t2.join();
+		t3.join();
 	}
 }
