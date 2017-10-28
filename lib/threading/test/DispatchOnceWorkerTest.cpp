be8fc7679867edd83e0808fa563525fa1cfa6f07
alexsuo <alex.suo@gmail.com>
2017-10-28 11:55:51 +0800

    Add test case.

--- lib/threading/test/DispatchOnceWorkerTest.cpp
+++ lib/threading/test/DispatchOnceWorkerTest.cpp
@@ -5,6 +5,8 @@
 
 #include <gtest/gtest.h>
 
+#include <CountDownLatch.hpp>
+
 #include "Dispatcher.hpp"
 #include "DispatchOnceWorker.hpp"
 
@@ -24,6 +26,29 @@ namespace mm
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		ASSERT_EQ(count, 1);
+	}
+
+	TEST(DispatchOnceWorkerTest, OnceCase)
+	{
+		HashDispatcher<std::int32_t, std::mutex> dispatcher(2);
+
+		std::int32_t id = 1;
+		std::int32_t count = 0;
+		CountDownLatch<> latch(1);
+
+		Runnable task = [&count, &latch] () { latch.wait(); ++count; };
+
+		DispatchOnceWorker<HashDispatcher<std::int32_t, std::mutex> > worker(id, task, dispatcher);
+		worker.dispatch();
+		worker.dispatch();
+		worker.dispatch();
+
+		latch.countDown();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		ASSERT_EQ(count, 1);
+
+		dispatcher.stop();
 	}
 
 }
