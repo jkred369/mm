6f5468b08b5d73ec170033d5dd0efdb97af2845e
alexsuo <alex.suo@gmail.com>
2017-11-02 21:22:13 +0800

    Implement an object pool.

--- lib/common/src/ObjectPool.hpp
+++ lib/common/src/ObjectPool.hpp
@@ -9,6 +9,7 @@
 #define LIB_COMMON_SRC_OBJECTPOOL_HPP_
 
 #include <atomic>
+#include <cassert>
 #include <mutex>
 #include <memory>
 
@@ -17,7 +18,7 @@ namespace mm
 	//
 	// This class implements an object pool for memory management.
 	//
-	template<typename ObjectType> class ObjectPool
+	template<typename ObjectType, typename Mutex = std::recursive_mutex> class ObjectPool
 	{
 	public:
 
@@ -35,9 +36,12 @@ namespace mm
 			//
 			virtual ~Recyclable()
 			{
-
+				Node* node = reinterpret_cast<Node*> (reinterpret_cast<char*> (this) - sizeof(void*));
+				node->pool->release(node);
 			}
 		};
+
+		friend class Recyclable;
 
 		// non-copyable class
 		ObjectPool(const ObjectPool& ) = delete;
@@ -48,7 +52,7 @@ namespace mm
 		//
 		// size : The fixed size of the object pool.
 		//
-		ObjectPool(size_t size) : size(size), pool(new Node[size]), first(pool[0]), last(pool[size - 1])
+		ObjectPool(size_t size, bool blocking = true) : blocking(blocking), size(size), pool(new Node[size]), first(pool[0])
 		{
 			for (size_t i = 0; i < size; ++i)
 			{
@@ -65,25 +69,66 @@ namespace mm
 		}
 
 		//
+		// Check if the pool is empty.
+		//
+		// return : true if the pool is empty.
+		//
+		bool empty()
+		{
+			return first.load() != nullptr;
+		}
+
+		//
 		// Get an object from the queue.
+		//
+		// return : The object constructed from the queue.
 		//
 		ObjectType* get()
 		{
 			Node* result;
+			Node* next;
 
 			while (true)
 			{
-				result = first;
-
-				if (first.compare_exchange_weak(first, first->next.load(),
-						std::memory_order_release, std::memory_order_relaxed))
+				if ((result = first.load()) == nullptr)
 				{
-					break;
+					if (blocking)
+					{
+						std::unique_lock<Mutex> lock(mutex);
+						condition.wait(lock);
+					}
+					else
+					{
+						return nullptr;
+					}
+				}
+
+				// must check here in case we back from condition signal
+				if (result != nullptr)
+				{
+					next = result->next.load();
+					if (first.compare_exchange_weak(result, next, std::memory_order_release, std::memory_order_relaxed))
+					{
+						break;
+					}
 				}
 			}
 
 			result.next.store(nullptr);
 			return new (result.objectBuffer) ObjectType();
+		}
+
+		//
+		// Release an object to the pool. The object must be previously allocated by this pool, else behavior is undefined.
+		//
+		// object : The object to be released.
+		//
+		void release(ObjectType* object)
+		{
+			Node* node = reinterpret_cast<Node*> (reinterpret_cast<char*> (this) - sizeof(void*));
+			assert(reinterpret_cast<ObjectPool*> (node) == this);
+
+			release(node);
 		}
 
 	protected:
@@ -97,16 +142,57 @@ namespace mm
 			ObjectPool* pool;
 
 			// Buffer for the object.
-			char objectBuffer[ sizeof (ObjectType) ];
+			char objectBuffer[ sizeof(ObjectType) ];
 
 			// Pointer to next node.
 			std::atomic<Node*> next;
 		};
 
+		//
+		// Release an object into the queue. The node must be originally from the buffer. Otherwise this method is a no-op.
+		//
+		// node : The node to be released.
+		//
+		void release(Node* node)
+		{
+			// sanity check
+			if (node < pool[0] || node > pool[size - 1])
+			{
+				delete node;
+				return;
+			}
+
+			// make sure the fields are correct.
+			node->pool = this;
+
+			// insertion
+			Node* firstValue;
+			while (true)
+			{
+				firstValue = first.load();
+				node->next.store(firstValue);
+
+				// move the last then append
+				if (first.compare_exchange_weak(firstValue, node, std::memory_order_release, std::memory_order_relaxed))
+				{
+					break;
+				}
+			}
+
+			// signal where needed
+			if (blocking && node == first.load())
+			{
+				condition.notify_all();
+			}
+		}
+
 	private:
 
+		// Flag if this pool should block on empty.
+		const bool blocking;
+
 		// Size of the object pool.
-		size_t size;
+		const size_t size;
 
 		// The pool of objects.
 		Node* const pool;
@@ -114,8 +200,11 @@ namespace mm
 		// Pointer to the first node available.
 		std::atomic<Node*> first;
 
-		// Pointer to the last node.
-		std::atomic<Node*> last;
+		// Lock for blocking if needed
+		Mutex mutex;
+
+		// Conditoin for empty
+		std::condition_variable_any condition;
 	};
 }
 
---
+++ lib/common/test/ObjectPoolTest.cpp
@@ -0,0 +1,31 @@
+#include <cstdint>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include "ObjectPool.hpp"
+
+namespace mm
+{
+	TEST(ObjectPoolTest, DummyCase)
+	{
+		ObjectPool<std::int64_t> pool(1, false);
+		ASSERT_TRUE(!pool.empty());
+
+		std::int64_t* value = pool.get();
+		ASSERT_TRUE(value != nullptr);
+		ASSERT_TRUE(pool.empty());
+		ASSERT_TRUE(pool.get() == nullptr);
+
+		pool.release(value);
+		ASSERT_TRUE(!pool.empty());
+
+		std::int64_t* value = pool.get();
+		ASSERT_TRUE(value != nullptr);
+		ASSERT_TRUE(pool.empty());
+		ASSERT_TRUE(pool.get() == nullptr);
+	}
+
+}
