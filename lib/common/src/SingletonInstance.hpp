001f6912ac3958d559c7043cb957f5104730576d
alexsuo <alex.suo@gmail.com>
2018-03-14 21:42:40 +0800

    Implementing subscription.

---
+++ lib/common/src/SingletonInstance.hpp
@@ -0,0 +1,74 @@
+/*
+ * SingletonInstance.hpp
+ *
+ *  Created on: Mar 14, 2018
+ *      Author: suoalex
+ */
+
+#ifndef LIB_COMMON_SRC_SINGLETONINSTANCE_HPP_
+#define LIB_COMMON_SRC_SINGLETONINSTANCE_HPP_
+
+#include <functional>
+#include <unordered_set>
+
+#include <boost/thread/locks.hpp>
+#include <boost/thread/shared_mutex.hpp>
+
+namespace mm
+{
+	//
+	// This class defines a singleton instance set where each constructed instance must be a singleton
+	// within the same process.
+	//
+	// The class requires its instances to have a proper hash function implemented.
+	//
+	template<
+		typename ValueType,
+		typename Hash = std::hash<ValueType>
+	> class SingletonInstance
+	{
+	public:
+
+		//
+		// Get the singleton instance for the value type class on the process.
+		//
+		// In case the value is requested the first time, create and store it.
+		//
+		// params : The parameter sets for the constructor of the value type.
+		//
+		// return : The singleton for the value type given the params for its constructor.
+		//
+		template<typename ... Params> static const ValueType& getInstance(Params ... params)
+		{
+			ValueType value(&params...);
+
+			{
+				boost::upgrade_lock<boost::shared_mutex> lock(MUTEX);
+
+				// insert if not found
+				std::unordered_set<ValueType>::iterator it = INSTANCES.find(value);
+				if (it == INSTANCES.end())
+				{
+					boost::upgrade_to_unique_lock<boost::shared_mutex> uniqueLock(lock);
+
+					INSTANCES.insert(value);
+					it = INSTANCES.find(value);
+				}
+
+				return *it;
+			}
+		}
+
+	private:
+
+		// The set of instances.
+		static std::unordered_set<ValueType> INSTANCES;
+
+		// The mutex for accessing the instances.
+		static boost::shared_mutex MUTEX;
+	};
+}
+
+
+
+#endif /* LIB_COMMON_SRC_SINGLETONINSTANCE_HPP_ */
---
+++ lib/messaging/src/Subscription.hpp
@@ -0,0 +1,82 @@
+/*
+ * Subscription.hpp
+ *
+ *  Created on: Mar 14, 2018
+ *      Author: suoalex
+ */
+
+#ifndef LIB_MESSAGING_SRC_SUBSCRIPTION_HPP_
+#define LIB_MESSAGING_SRC_SUBSCRIPTION_HPP_
+
+#include <ISubscription.hpp>
+#include <SingletonInstance.hpp>
+
+namespace mm
+{
+	//
+	// The subscription class provides the default implementation for the subscription interface.
+	//
+	class Subscription : BaseSubscription, SingletonInstance<Subscription>
+	{
+	public:
+
+		virtual ~Subscription()
+		{
+		}
+
+		virtual SourceType getSourceType() const override
+		{
+			return sourceType;
+		}
+
+		virtual DataType getDataType() const override
+		{
+			return dataType;
+		}
+
+		virtual const std::int64_t& getKey() const override
+		{
+			return key;
+		}
+
+		//
+		// Implement custom comparison for the subscription.
+		//
+		// rhs : The other subscription instance.
+		//
+		// return : true if the 2 subscription objects are identical.
+		//
+		bool operator == (const Subscription& rhs) const
+		{
+			return sourceType == rhs.sourceType &&
+					dataType == rhs.dataType &&
+					key == rhs.key;
+		}
+
+	private:
+
+		// Source type
+		SourceType sourceType;
+
+		// Data type
+		DataType dataType;
+
+		// Key of the subscription
+		std::int64_t key;
+	};
+}
+
+namespace std
+{
+	template<> struct hash<mm::Subscription>
+	{
+		std::size_t operator()(const mm::Subscription& value) const
+		{
+			return std::hash<std::int16_t> (value.sourceType) ^
+					std::hash<std::int16_t> (value.dataType) ^
+					std::hash<std::int64_t> (value.key);
+		}
+	};
+}
+
+#endif /* LIB_MESSAGING_SRC_SUBSCRIPTION_HPP_ */
