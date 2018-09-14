a922220d48bb93a62fad8a9b7d01386e1c1493f6
alexsuo <alex.suo@gmail.com>
2018-09-14 22:23:37 +0800

    Add missing files.

---
+++ lib/messaging/src/FieldDefinition.hpp
@@ -0,0 +1,21 @@
+/*
+ * FieldDefinition.hpp
+ *
+ *  Created on: Sep 14, 2018
+ *      Author: suoalex
+ */
+
+#ifndef LIB_MESSAGING_SRC_FIELDDEFINITION_HPP_
+#define LIB_MESSAGING_SRC_FIELDDEFINITION_HPP_
+
+#include <FixedSizeString.hpp>
+
+namespace mm
+{
+	// defines all the field types for specific fields.
+	typedef FixedSizeString<31> InstrumentIDType;
+}
+
+
+
+#endif /* LIB_MESSAGING_SRC_FIELDDEFINITION_HPP_ */
---
+++ lib/messaging/src/Side.hpp
@@ -0,0 +1,23 @@
+/*
+ * Side.hpp
+ *
+ *  Created on: Sep 14, 2018
+ *      Author: suoalex
+ */
+
+#ifndef LIB_MESSAGING_SRC_SIDE_HPP_
+#define LIB_MESSAGING_SRC_SIDE_HPP_
+
+namespace mm
+{
+	//
+	// The enum for market data side.
+	//
+	enum class Side : int
+	{
+		BID = 0,
+		ASK = 1
+	};
+}
+
+#endif /* LIB_MESSAGING_SRC_SIDE_HPP_ */
