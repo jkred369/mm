294670447460dd16a2f672427d44ba84dcfdf027
alexsuo <alex.suo@gmail.com>
2018-09-15 09:07:27 +0800

    Add util class.

---
+++ lib/bus/src/ContextConsumer.hpp
@@ -0,0 +1,57 @@
+/*
+ * ContextConsumer.hpp
+ *
+ *  Created on: Sep 15, 2018
+ *      Author: suoalex
+ */
+
+#ifndef LIB_BUS_SRC_CONTEXTCONSUMER_HPP_
+#define LIB_BUS_SRC_CONTEXTCONSUMER_HPP_
+
+#include <cstdint>
+#include <memory>
+
+#include <IConsumer.hpp>
+
+namespace mm
+{
+	//
+	// This class performs a context switching for consuming a message.
+	//
+	template<typename Message, typename Key = std::int32_t, typename Dispatcher> class ContextConsumer : public IConsumer<Message>
+	{
+	public:
+
+		ContextConsumer(const Key& key,
+				std::shared_ptr<Dispatcher>& dispatcher,
+				std::shared_ptr<IConsumer<Message> >& consumer) :
+			key(key),
+			dispatcher(dispatcher),
+			consumer(consumer)
+		{
+		}
+
+		virtual ~ContextConsumer() {}
+
+		virtual void consume(const std::shared_ptr<const Message>& message) override
+		{
+			dispatcher->submit(key, [message]() {
+				consumer->consume(message);
+			});
+		}
+
+	private:
+
+		// The key to assign task on.
+		const Key key;
+
+		// The dispatcher.
+		std::shared_ptr<Dispatcher> dispatcher;
+
+		// The wrapped consumer.
+		std::shared_ptr<IConsumer<Message> > consumer;
+	};
+}
+
+
+#endif /* LIB_BUS_SRC_CONTEXTCONSUMER_HPP_ */
