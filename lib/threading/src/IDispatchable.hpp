8d3790095a4ed029804d868a54a80817de5be40e
alexsuo <alex.suo@gmail.com>
2018-09-18 21:11:45 +0800

    Add missing files.

---
+++ lib/threading/src/IDispatchable.hpp
@@ -0,0 +1,33 @@
+/*
+ * IDispatchable.hpp
+ *
+ *  Created on: Sep 18, 2018
+ *      Author: suoalex
+ */
+
+#ifndef LIB_THREADING_SRC_IDISPATCHABLE_HPP_
+#define LIB_THREADING_SRC_IDISPATCHABLE_HPP_
+
+#include <DispatchKey.hpp>
+
+namespace mm
+{
+	//
+	// defines the dispatchable interface.
+	//
+	struct IDispatchable
+	{
+		virtual ~IDispatchable() {}
+
+		//
+		// Get the key for the dispatchable.
+		//
+		// return : The dispatch key for this object.
+		//
+		virtual KeyType getKey() const = 0;
+	};
+}
+
+
+
+#endif /* LIB_THREADING_SRC_IDISPATCHABLE_HPP_ */
