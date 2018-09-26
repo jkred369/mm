89648e7b0c96b1ea1082d6a682b4163a326444ae
alexsuo <alex.suo@gmail.com>
2018-09-26 23:04:41 +0800

    Add missing file.

---
+++ lib/bus/test/PublisherAdapterTest.cpp
@@ -0,0 +1,55 @@
+/*
+ * PublisherAdapterTest.cpp
+ *
+ *  Created on: Sep 26, 2018
+ *      Author: suoalex
+ */
+
+#include <atomic>
+#include <cstdint>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include <Dispatcher.hpp>
+#include <Subscription.hpp>
+
+#include "PublisherAdapter.hpp"
+
+namespace mm
+{
+	struct DummyMessage
+	{
+		DummyMessage() : counter(0)
+		{
+		}
+
+		std::atomic<int> counter;
+	};
+
+	struct DummyConsumer : IConsumer<DummyMessage>
+	{
+		virtual void consume(const std::shared_ptr<const DummyMessage>& message) override
+		{
+			++counter;
+		}
+
+		std::atomic<int> counter;
+	};
+
+	TEST(PublisherAdapterTest, DummyCase)
+	{
+		std::shared_ptr<Dispatcher> dispatcher = std::make_shared<Dispatcher>();
+
+		PublisherAdapter<DummyMessage> adapter(dispatcher);
+
+		const Subscription subscription = {SourceType::FEMAS_MARKET_DATA, DataType::MARKET_DATA, 1};
+		adapter.publish(subscription, std::make_shared<DummyMessage>());
+	}
+
+}
+
+
+
