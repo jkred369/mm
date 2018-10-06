913e80cd9fe8207c8a7a15d8dd1fe259fbc2921c
alexsuo <alex.suo@gmail.com>
2018-10-06 14:39:19 +0800

    More test cases passed.

--- lib/order/test/OrderTest.cpp
+++ lib/order/test/OrderTest.cpp
@@ -204,5 +204,154 @@ namespace mm
 		}
 	}
 
+	TEST(OrderTest, SendLiveTradeCase)
+	{
+		Dispatcher dispatcher(2);
+		DummyPublisher publisher(dispatcher);
+		DummyExchange exchange;
+
+		TestOrder order(publisher, publisher, exchange);
+
+		// send
+		{
+			std::shared_ptr<OrderMessage> ptr = std::make_shared<OrderMessage> ();
+			OrderMessage& message = *ptr;
+
+			message.orderId = 1;
+			message.instrumentId = 2;
+			message.price = 20000.0;
+			message.totalQty = 1;
+			message.side = Side::BID;
+			message.status = OrderStatus::LIVE;
+
+			order.consume(ptr);
+
+			ASSERT_TRUE(order.getOrderId() == message.orderId);
+			ASSERT_TRUE(order.getInstrumentId() == message.instrumentId);
+			ASSERT_TRUE(order.getPrice() == message.price);
+			ASSERT_TRUE(order.getTotalQty() == message.totalQty);
+			ASSERT_TRUE(order.getSide() == message.side);
+			ASSERT_TRUE(order.getStatus() == OrderStatus::PENDING_ACK);
+
+			ASSERT_TRUE(!exchange.statusMap.empty());
+			ASSERT_TRUE(exchange.statusMap.find(message.orderId) != exchange.statusMap.end());
+			ASSERT_TRUE(exchange.statusMap[message.orderId] == OrderStatus::LIVE);
+
+			ASSERT_TRUE(!publisher.messageMap.empty());
+
+			{
+				Subscription sub = {SourceType::ALL, DataType::ORDER_SUMMARY, message.orderId};
+				ASSERT_TRUE(publisher.messageMap.find(sub) != publisher.messageMap.end());
+
+				std::shared_ptr<const OrderSummaryMessage> summaryPtr = publisher.messageMap[sub];
+				ASSERT_TRUE(summaryPtr);
+
+				const OrderSummaryMessage& summary = *summaryPtr;
+				ASSERT_TRUE(summary.orderId == message.orderId);
+				ASSERT_TRUE(summary.instrumentId == message.instrumentId);
+				ASSERT_TRUE(summary.price == message.price);
+				ASSERT_TRUE(summary.totalQty == message.totalQty);
+				ASSERT_TRUE(summary.side == message.side);
+				ASSERT_TRUE(summary.status == OrderStatus::PENDING_ACK);
+			}
+		}
+
+		// live
+		{
+			std::shared_ptr<ExecutionReportMessage> ptr = std::make_shared<ExecutionReportMessage> ();
+			ExecutionReportMessage& message = *ptr;
+
+			message.orderId = 1;
+			message.status = OrderStatus::LIVE;
+			message.price = 20000.0;
+			message.openQty = 1;
+			message.tradedQty = 0;
+
+			order.consume(ptr);
+
+			ASSERT_TRUE(!exchange.statusMap.empty());
+			ASSERT_TRUE(exchange.statusMap.find(message.orderId) != exchange.statusMap.end());
+			ASSERT_TRUE(exchange.statusMap[message.orderId] == OrderStatus::LIVE);
+
+			ASSERT_TRUE(!publisher.messageMap.empty());
+
+			{
+				Subscription sub = {SourceType::ALL, DataType::ORDER_SUMMARY, message.orderId};
+				ASSERT_TRUE(publisher.messageMap.find(sub) != publisher.messageMap.end());
+
+				std::shared_ptr<const OrderSummaryMessage> summaryPtr = publisher.messageMap[sub];
+				ASSERT_TRUE(summaryPtr);
+
+				const OrderSummaryMessage& summary = *summaryPtr;
+				ASSERT_TRUE(summary.orderId == message.orderId);
+				ASSERT_TRUE(summary.instrumentId == 2);
+				ASSERT_TRUE(summary.price == 20000.0);
+				ASSERT_TRUE(summary.totalQty == 1);
+				ASSERT_TRUE(summary.side == Side::BID);
+				ASSERT_TRUE(summary.status == OrderStatus::LIVE);
+			}
+		}
+
+		// trade
+		{
+			std::shared_ptr<ExecutionReportMessage> ptr = std::make_shared<ExecutionReportMessage> ();
+			ExecutionReportMessage& message = *ptr;
+
+			message.orderId = 1;
+			message.instrumentId = 2;
+			message.executionId = 20001312;
+			message.avgTradedPrice = 19999.0;
+			message.execPrice = 19999.0;
+			message.price = 20000.0;
+			message.execQty = 1;
+			message.status = OrderStatus::FULL_FILLED;
+			message.openQty = 0;
+			message.side = Side::BID;
+			message.tradedQty = 1;
+
+			order.consume(ptr);
+
+			// exchange interface isn't touched ...
+			ASSERT_TRUE(!exchange.statusMap.empty());
+			ASSERT_TRUE(exchange.statusMap.find(message.orderId) != exchange.statusMap.end());
+			ASSERT_TRUE(exchange.statusMap[message.orderId] == OrderStatus::LIVE);
+
+			ASSERT_TRUE(!publisher.messageMap.empty());
+
+			{
+				Subscription sub = {SourceType::ALL, DataType::ORDER_SUMMARY, message.orderId};
+				ASSERT_TRUE(publisher.messageMap.find(sub) != publisher.messageMap.end());
+
+				std::shared_ptr<const OrderSummaryMessage> summaryPtr = publisher.messageMap[sub];
+				ASSERT_TRUE(summaryPtr);
+
+				const OrderSummaryMessage& summary = *summaryPtr;
+				ASSERT_TRUE(summary.orderId == message.orderId);
+				ASSERT_TRUE(summary.instrumentId == 2);
+				ASSERT_TRUE(summary.price == 20000.0);
+				ASSERT_TRUE(summary.totalQty == 1);
+				ASSERT_TRUE(summary.tradedQty == 1);
+				ASSERT_TRUE(summary.avgTradedPrice == 19999.0);
+				ASSERT_TRUE(summary.side == Side::BID);
+				ASSERT_TRUE(summary.status == OrderStatus::FULL_FILLED);
+			}
+
+			{
+				Subscription sub = {SourceType::ALL, DataType::TRADE, message.executionId};
+				ASSERT_TRUE(publisher.tradeMap.find(sub) != publisher.tradeMap.end());
+
+				std::shared_ptr<const TradeMessage> tradePtr = publisher.tradeMap[sub];
+				ASSERT_TRUE(tradePtr);
+
+				const TradeMessage& trade = *tradePtr;
+				ASSERT_TRUE(trade.orderId == message.orderId);
+				ASSERT_TRUE(trade.instrumentId == 2);
+				ASSERT_TRUE(trade.qty == 1);
+				ASSERT_TRUE(trade.price == 19999.0);
+				ASSERT_TRUE(trade.side == Side::BID);
+			}
+		}
+	}
+
 }
 
