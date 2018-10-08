1bbea3846a8eaee1f921c06c1153aeb2ce38cbdb
alexsuo <alex.suo@gmail.com>
2018-10-08 20:57:02 +0800

    Add a simulation order session.

---
+++ lib/simulation/src/RandomOrderSession.cpp
@@ -0,0 +1,101 @@
+/*
+ * RandomOrderSession.cpp
+ *
+ *  Created on: Oct 8, 2018
+ *      Author: suoalex
+ */
+
+#include "RandomOrderSession.hpp"
+
+mm::Logger mm::RandomOrderSession::logger;
+
+namespace mm
+{
+	RandomOrderSession::RandomOrderSession(ServiceContext& serviceContext, const std::string productServiceName)
+	{
+		static_assert(BID >= 0, "Bid index must be positive.");
+		static_assert(ASK >= 0, "Ask index must be positive.");
+
+		// create symbol mapping
+		{
+			ProductService* productService;
+			if (!serviceContext.getService(productServiceName, productService))
+			{
+				LOGERR("Order session failed to get product service from service context");
+				throw std::runtime_error("Failed to create Random order session. Cannot find product service with name: " + productServiceName);
+			}
+
+			productService->initSnapshot(this);
+			LOGINFO("ID set created with {} products.", supportedSet.size());
+		}
+	}
+
+	RandomOrderSession::~RandomOrderSession()
+	{
+	}
+
+	void RandomOrderSession::consume(const std::shared_ptr<const Product>& message)
+	{
+		supportedSet.insert(message->getContent().id);
+	}
+
+	void RandomOrderSession::sendOrder(const std::shared_ptr<OrderMessage>& message)
+	{
+		if (liveOrderMap.find(message->id) != liveOrderMap.end())
+		{
+			LOGERR("Failed to operate on order with id: {}, requested status: {}", message->orderId, toValue(message->status));
+			return;
+		}
+
+		RdtscTimer timer;
+		const OrderMessage copy = *message;
+
+		scheduler.schedule(dispatchKey, [this, copy] () {
+
+			std::shared_ptr<ExecutionReportMessage> reportPtr = executionReportPool.getShared();
+			ExecutionReportMessage& report = *reportPtr;
+
+			report.orderId = copy.orderId;
+			report.instrumentId = copy.instrumentId;
+			report.openQty = copy.totalQty;
+			report.price = copy.price;
+			report.side = copy.side;
+			report.status = OrderStatus::LIVE;
+
+			const Subscription subscription = {SourceType::ALL, DataType::EXEC_REPORT, copy.strategyId};
+			this->publish(subscription, reportPtr);
+
+		}, std::chrono::milliseconds(2));
+
+		return true;
+	}
+
+	void RandomOrderSession::cancel(const std::shared_ptr<OrderMessage>& message)
+	{
+		if (liveOrderMap.find(message->id) == liveOrderMap.end())
+		{
+			LOGERR("Failed to find order with id: {}, requested status: {}", message->orderId, toValue(message->status));
+			return;
+		}
+
+		RdtscTimer timer;
+		const OrderMessage copy = *message;
+
+		scheduler.schedule(dispatchKey, [this, copy, status] () {
+
+			std::shared_ptr<ExecutionReportMessage> reportPtr = executionReportPool.getShared();
+			ExecutionReportMessage& report = *reportPtr;
+
+			report.orderId = copy.orderId;
+			report.instrumentId = copy.instrumentId;
+			report.status = OrderStatus::CANCELLED;
+
+			const Subscription subscription = {SourceType::ALL, DataType::EXEC_REPORT, copy.strategyId};
+			this->publish(subscription, reportPtr);
+
+		}, std::chrono::milliseconds(2));
+
+		return true;
+	}
+
+}
