24e5e9b9d1892f7852d4c151d2596a38083bbf24
alexsuo <alex.suo@gmail.com>
2018-10-17 15:06:55 +0800

    Adding product download session, still need work.

---
+++ lib/femas/src/FemasProductDownloadSession.cpp
@@ -0,0 +1,358 @@
+/*
+ * FemasProductDownloadSession.cpp
+ *
+ *  Created on: Oct 17, 2018
+ *      Author: suoalex
+ */
+
+#include <EnumType.hpp>
+#include <NativeDefinition.hpp>
+#include <ProductService.hpp>
+#include <StringUtil.hpp>
+
+#include "FemasProductDownloadSession.hpp"
+
+mm::Logger mm::FemasProductDownloadSession::logger;
+
+namespace mm
+{
+	FemasProductDownloadSession::FemasProductDownloadSession(
+			const KeyType dispatchKey,
+			const std::string serviceName,
+			ServiceContext& serviceContext,
+			const std::string productServiceName,
+			const FemasUserDetail& userDetail) :
+		DispatchableService(dispatchKey, serviceName, serviceContext),
+		userDetail(userDetail),
+		dispatcher(serviceContext.getDispatcher()),
+		session(CUstpFtdcTraderApi::CreateFtdcTraderApi()),
+		productService(nullptr),
+		stopFlag(false),
+		requestId(0)
+	{
+		// Seeking product service.
+		if (!serviceContext.getService(productServiceName, productService))
+		{
+			LOGERR("Market data session failed to get product service from service context");
+			throw std::runtime_error("Failed to create Femas product download session. Cannot find product service with name: " + productServiceName);
+		}
+
+		// basic wiring up - the API interface forced const_cast usage
+		session->RegisterSpi(this);
+
+		if (!userDetail.frontAddress.empty())
+		{
+			session->RegisterFront(const_cast<char*> (userDetail.frontAddress.c_str()));
+		}
+		else if (!userDetail.nameServer.empty())
+		{
+			session->RegisterNameServer(const_cast<char*> (userDetail.nameServer.c_str()));
+		}
+		else
+		{
+			throw std::invalid_argument("No front address and name server.");
+		}
+
+		if (!userDetail.certFileName.empty())
+		{
+			session->RegisterCertificateFile(
+					userDetail.certFileName.c_str(),
+					userDetail.keyFileName.c_str(),
+					userDetail.caFileName.c_str(),
+					userDetail.keyFilePassword.c_str());
+		}
+
+		session->Init();
+	}
+
+	FemasProductDownloadSession::~FemasProductDownloadSession()
+	{
+		stop();
+	}
+
+	bool FemasProductDownloadSession::start()
+	{
+		if (!DispatchableService::start())
+		{
+			return false;
+		}
+
+		// wait on init latch to make sure the connection is established
+		LOGINFO("Waiting for connection...")
+		initLatch.wait();
+
+		// login attempt
+		CUstpFtdcReqUserLoginField field;
+		std::memset(&field, 0, sizeof(field));
+
+		field.DataCenterID = userDetail.dataCenterId;
+
+		StringUtil::copy(field.BrokerID, userDetail.brokerId, sizeof(field.BrokerID));
+		StringUtil::copy(field.IPAddress, userDetail.ipAddress, sizeof(field.IPAddress));
+		StringUtil::copy(field.InterfaceProductInfo, userDetail.interfaceProductInfo, sizeof(field.InterfaceProductInfo));
+		StringUtil::copy(field.MacAddress, userDetail.macAddress, sizeof(field.MacAddress));
+		StringUtil::copy(field.Password, userDetail.password, sizeof(field.Password));
+		StringUtil::copy(field.ProtocolInfo, userDetail.protocolInfo, sizeof(field.ProtocolInfo));
+		StringUtil::copy(field.TradingDay, session->GetTradingDay(), sizeof(field.TradingDay));
+		StringUtil::copy(field.UserID, userDetail.userId, sizeof(field.UserID));
+		StringUtil::copy(field.UserProductInfo, userDetail.userProductInfo, sizeof(field.UserProductInfo));
+
+		const int result = session->ReqUserLogin(&field, ++requestId);
+
+		if (result == 0)
+		{
+			LOGINFO("Product download session logged in as {}", userDetail.userId);
+			return true;
+		}
+		else
+		{
+			LOGERR("Error loging as user {} with code: {}", userDetail.userId, result);
+			return false;
+		}
+
+		// querying the products
+		session->SubscribeUserTopic(USTP_TERT_RESUME);
+	}
+
+	void FemasProductDownloadSession::stop()
+	{
+		// sanity check
+		{
+			bool expected = false;
+			if (!stopFlag.compare_exchange_strong(expected, true))
+			{
+				return;
+			}
+		}
+
+		// attempt to logout but stop even if failed
+		{
+			CUstpFtdcReqUserLogoutField field;
+			StringUtil::copy(field.BrokerID, userDetail.brokerId, sizeof(field.BrokerID));
+			StringUtil::copy(field.UserID, userDetail.userId, sizeof(field.UserID));
+
+			const int result = session->ReqUserLogout(&field, ++requestId);
+			if (result != 0)
+			{
+				LOGERR("Error logout user: {} with code: {}", userDetail.userId, result);
+			}
+		}
+
+		// actually stop the service
+		// it's commented that session->join() isn't stable?
+		session->RegisterSpi(nullptr);
+		session->Release();
+		session = nullptr;
+
+		// delegate
+		DispatchableService::stop();
+	}
+
+	void FemasProductDownloadSession::OnFrontConnected()
+	{
+		initLatch.countDown();
+		LOGDEBUG("Product download session connected.");
+	}
+
+	void FemasProductDownloadSession::OnFrontDisconnected(int reason)
+	{
+		LOGDEBUG("Product download session disconnected on {}, auto-reconnecting", reason);
+	}
+
+	void FemasProductDownloadSession::OnHeartBeatWarning(int timeLapse)
+	{
+		LOGWARN("No heart beat in {} seconds.", timeLapse);
+	}
+
+	void FemasProductDownloadSession::OnPackageStart(int topicID, int sequenceNo)
+	{
+		LOGTRACE("Received packet {} on topic {}", sequenceNo, topicID);
+	}
+
+	void FemasProductDownloadSession::OnPackageEnd(int topicID, int sequenceNo)
+	{
+		LOGTRACE("Finished packet {} on topic {}", sequenceNo, topicID);
+	}
+
+	void FemasProductDownloadSession::OnRspError(CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+		LOGERR("Error on request {} : {}, {}", requestID, info->ErrorID, info->ErrorMsg);
+	}
+
+	void FemasProductDownloadSession::OnRspUserLogin(CUstpFtdcRspUserLoginField *userLogin, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+		if (info->ErrorID == 0)
+		{
+			tradingDate = userLogin->TradingDay;
+			LOGINFO("User {} logged in successfully on date {}", userLogin->UserID, tradingDate);
+		}
+		else
+		{
+			LOGERR("Login attempt failed. User: {}, error: {}, {}", userLogin->UserID, info->ErrorID, info->ErrorMsg);
+		}
+	}
+
+	void FemasProductDownloadSession::OnRspUserLogout(CUstpFtdcRspUserLogoutField *userLogout, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+		if (info->ErrorID == 0)
+		{
+			LOGINFO("User {} logged out successfully", userLogout->UserID);
+		}
+		else
+		{
+			LOGERR("Logout attempt failed. User: {}, error: {}, {}", userLogout->UserID, info->ErrorID, info->ErrorMsg);
+		}
+	}
+
+	void FemasProductDownloadSession::OnRspSubscribeTopic(CUstpFtdcDisseminationField *dissemination, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+		if (info->ErrorID == 0)
+		{
+			LOGINFO("Subscribed to {}", dissemination->SequenceSeries);
+		}
+		else
+		{
+			LOGERR("Error subscribing to {}: {}, {}", dissemination->SequenceSeries, info->ErrorID, info->ErrorMsg);
+		}
+	}
+
+	void FemasProductDownloadSession::OnRspQryTopic(CUstpFtdcDisseminationField *dissemination, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+		if (info->ErrorID == 0)
+		{
+			LOGINFO("Queried topic {}", dissemination->SequenceSeries);
+		}
+		else
+		{
+			LOGERR("Error querying {}: {}, {}", dissemination->SequenceSeries, info->ErrorID, info->ErrorMsg);
+		}
+	}
+
+	void FemasProductDownloadSession::OnRspUserPasswordUpdate(CUstpFtdcUserPasswordUpdateField *userPasswordUpdate, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+		if (info->ErrorID == 0)
+		{
+			LOGINFO("User {} password updated from {} to {}", userPasswordUpdate->UserID, userPasswordUpdate->OldPassword, userPasswordUpdate->NewPassword);
+		}
+		else
+		{
+			LOGERR("User {} password update failed: {}, {}", userPasswordUpdate->UserID, info->ErrorID, info->ErrorMsg);
+		}
+	}
+
+	void FemasProductDownloadSession::OnRspOrderInsert(CUstpFtdcInputOrderField *inputOrder, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspOrderAction(CUstpFtdcOrderActionField *orderAction, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRtnFlowMessageCancel(CUstpFtdcFlowMessageCancelField *flowMessageCancel)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRtnTrade(CUstpFtdcTradeField *trade)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRtnOrder(CUstpFtdcOrderField *order)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnErrRtnOrderInsert(CUstpFtdcInputOrderField *inputOrder, CUstpFtdcRspInfoField *info)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnErrRtnOrderAction(CUstpFtdcOrderActionField *orderAction, CUstpFtdcRspInfoField *info)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRtnInstrumentStatus(CUstpFtdcInstrumentStatusField *instrumentStatus)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRtnInvestorAccountDeposit(CUstpFtdcInvestorAccountDepositResField *investorAccountDepositRes)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspQryOrder(CUstpFtdcOrderField *order, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspQryTrade(CUstpFtdcTradeField *trade, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspQryUserInvestor(CUstpFtdcRspUserInvestorField *userInvestor, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+	}
+
+	void FemasProductDownloadSession::OnRspQryTradingCode(CUstpFtdcRspTradingCodeField *tradingCode, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+	}
+
+	void FemasProductDownloadSession::OnRspQryInvestorAccount(CUstpFtdcRspInvestorAccountField *investorAccount, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspQryInstrument(CUstpFtdcRspInstrumentField *instrument, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspQryExchange(CUstpFtdcRspExchangeField *exchange, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+		if (info->ErrorID == 0)
+		{
+			const std::string femasExchangeId = exchange->ExchangeID;
+
+			dispatcher.submit(dispatchKey, [this, femasExchangeId] () {
+				exchangeId = femasExchangeId;
+				LOGINFO("Exchange ID set to: {}", exchangeId);
+
+				startLatch.countDown();
+			});
+		}
+		else
+		{
+			LOGERR("Error getting exchange ID with error: {}, {}", info->ErrorID, info->ErrorMsg);
+		}
+	}
+
+	void FemasProductDownloadSession::OnRspQryInvestorPosition(CUstpFtdcRspInvestorPositionField *investorPosition, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspQryComplianceParam(CUstpFtdcRspComplianceParamField *complianceParam, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspQryInvestorFee(CUstpFtdcInvestorFeeField *investorFee, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+	void FemasProductDownloadSession::OnRspQryInvestorMargin(CUstpFtdcInvestorMarginField *investorMargin, CUstpFtdcRspInfoField *info, int requestID, bool isLast)
+	{
+
+	}
+
+}
+
+
+
---
+++ lib/femas/src/FemasProductDownloadSession.hpp
@@ -0,0 +1,386 @@
+/*
+ * FemasProductDownloadSession.hpp
+ *
+ *  Created on: Oct 17, 2018
+ *      Author: suoalex
+ */
+
+#ifndef LIB_FEMAS_SRC_FEMASPRODUCTDOWNLOADSESSION_HPP_
+#define LIB_FEMAS_SRC_FEMASPRODUCTDOWNLOADSESSION_HPP_
+
+#include <atomic>
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include <femas/USTPFtdcTraderApi.h>
+
+#include <CountDownLatch.hpp>
+#include <DispatchableService.hpp>
+#include <EnumType.hpp>
+#include <IService.hpp>
+#include <Logger.hpp>
+#include <Product.hpp>
+#include <ProductMessage.hpp>
+#include <ProductService.hpp>
+
+#include "FemasUserDetail.hpp"
+
+namespace mm
+{
+	//
+	// This class defines a product download session on femas for retrieving all products from exchange.
+	//
+	class FemasProductDownloadSession :
+			public DispatchableService,
+			public CUstpFtdcTraderSpi
+	{
+	public:
+
+		//
+		// Constructor.
+		//
+		// dispatchKey : The dispatch key for order dispatching.
+		// serviceName : The service name in context.
+		// serviceContext : The service context.
+		// productServiceName : The product service name.
+		// userDetail : The configuration details.
+		//
+		FemasProductDownloadSession(
+				const KeyType dispatchKey,
+				const std::string serviceName,
+				ServiceContext& serviceContext,
+				const std::string productServiceName,
+				const FemasUserDetail& userDetail);
+
+		// virtual destructor.
+		virtual ~FemasProductDownloadSession();
+
+		//
+		// service interface.
+		//
+		virtual bool start() override;
+		virtual void stop() override;
+
+		//
+		// Fired when the session is connected.
+		//
+		virtual void OnFrontConnected() override;
+
+		//
+		// Fired when the session is disconnected.
+		//
+		// reason : The reason number.
+		//
+		virtual void OnFrontDisconnected(int reason) override;
+
+		//
+		// Fired when the heart beat got error.
+		//
+		// timeLapse : The time passed.
+		//
+		virtual void OnHeartBeatWarning(int timeLapse) override;
+
+		//
+		// Fired when the packet start is received.
+		//
+		// topicID : The topic on which the packet is received.
+		// sequenceNo : The sequence number.
+		//
+		virtual void OnPackageStart(int topicID, int sequenceNo) override;
+
+		//
+		// Fired when the packet end is received.
+		//
+		// topicID : The topic on which the packet is received.
+		// sequenceNo : The sequence number.
+		//
+		virtual void OnPackageEnd(int topicID, int sequenceNo) override;
+
+		//
+		// Fired when the response got error.
+		//
+		// info : Structure with details.
+		// requestID : The request got the error.
+		// isLast : Flag if this is the last request.
+		//
+		virtual void OnRspError(CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for the login response.
+		//
+		// userLogin : details for the login
+		// info : information details.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspUserLogin(CUstpFtdcRspUserLoginField *userLogin, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for the logout response.
+		//
+		// userLogout : details for the logout
+		// info : information details.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspUserLogout(CUstpFtdcRspUserLogoutField *userLogout, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for the subscription response.
+		//
+		// dissemination : details for the subscription.
+		// info : information details.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspSubscribeTopic(CUstpFtdcDisseminationField *dissemination, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for the topic query response.
+		//
+		// dissemination : details for the subscription.
+		// info : information details.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryTopic(CUstpFtdcDisseminationField *dissemination, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for user password updated.
+		//
+		// userPasswordUpdate : The password change detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspUserPasswordUpdate(CUstpFtdcUserPasswordUpdateField *userPasswordUpdate, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for order insertion.
+		//
+		// inputOrder : The order insertion detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspOrderInsert(CUstpFtdcInputOrderField *inputOrder, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for order action.
+		//
+		// orderAction : The order action detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspOrderAction(CUstpFtdcOrderActionField *orderAction, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for flow message canceled.
+		//
+		// flowMessageCancel : Detail of the canceled flow.
+		//
+		virtual void OnRtnFlowMessageCancel(CUstpFtdcFlowMessageCancelField *flowMessageCancel) override;
+
+		//
+		// Fired for trade.
+		//
+		// trade : Detail of the trade.
+		//
+		virtual void OnRtnTrade(CUstpFtdcTradeField *trade) override;
+
+		//
+		// Fired for order.
+		//
+		// order : Detail of the order.
+		//
+		virtual void OnRtnOrder(CUstpFtdcOrderField *order) override;
+
+		//
+		// Fired for order insertion.
+		//
+		// inputOrder : The input order detail.
+		// info : The returned info object.
+		//
+		virtual void OnErrRtnOrderInsert(CUstpFtdcInputOrderField *inputOrder, CUstpFtdcRspInfoField *info) override;
+
+		//
+		// Fired for order action.
+		//
+		// orderAction : The order action detail.
+		// info : The returned info object.
+		//
+		virtual void OnErrRtnOrderAction(CUstpFtdcOrderActionField *orderAction, CUstpFtdcRspInfoField *info) override;
+
+		//
+		// Fired for instrument status update.
+		//
+		// instrumentStatus : The instrument status.
+		//
+		virtual void OnRtnInstrumentStatus(CUstpFtdcInstrumentStatusField *instrumentStatus) override;
+
+		//
+		// Fired for account deposit update.
+		//
+		// investorAccountDepositRes : The acciont deposit detail.
+		//
+		virtual void OnRtnInvestorAccountDeposit(CUstpFtdcInvestorAccountDepositResField *investorAccountDepositRes) override;
+
+		//
+		// Fired for order query.
+		//
+		// orderAction : The order action detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryOrder(CUstpFtdcOrderField *order, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for trade query.
+		//
+		// trade : The trade detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryTrade(CUstpFtdcTradeField *trade, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for user query
+		//
+		// userInvestor : The user detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryUserInvestor(CUstpFtdcRspUserInvestorField *userInvestor, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for trading code query
+		//
+		// tradingCode : The trading code detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryTradingCode(CUstpFtdcRspTradingCodeField *tradingCode, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for investor account detail.
+		//
+		// investorAccount : The account detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryInvestorAccount(CUstpFtdcRspInvestorAccountField *investorAccount, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for instrument detail.
+		//
+		// instrument : The instrument detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryInstrument(CUstpFtdcRspInstrumentField *instrument, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for exchange detail.
+		//
+		// exchange : The exchange detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryExchange(CUstpFtdcRspExchangeField *exchange, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for position detail.
+		//
+		// investorPosition : The position detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryInvestorPosition(CUstpFtdcRspInvestorPositionField *investorPosition, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for compliance detail.
+		//
+		// complianceParam : The compliance detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryComplianceParam(CUstpFtdcRspComplianceParamField *complianceParam, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for investor fee detail.
+		//
+		// investorFee : The fee detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryInvestorFee(CUstpFtdcInvestorFeeField *investorFee, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+		//
+		// Fired for investor margin detail.
+		//
+		// investorMargin : The margin detail.
+		// info : The returned info object.
+		// requestID : The request ID.
+		// isLast : Flag if this is the last response.
+		//
+		virtual void OnRspQryInvestorMargin(CUstpFtdcInvestorMarginField *investorMargin, CUstpFtdcRspInfoField *info, int requestID, bool isLast) override;
+
+	private:
+
+		// The logger for this class.
+		static Logger logger;
+
+		// The login user detail.
+		const FemasUserDetail userDetail;
+
+		// The dispatcher.
+		Dispatcher& dispatcher;
+
+		// The actual API session.
+		// Note that we cannot use unique_ptr etc here as the destructor is protected.
+		CUstpFtdcTraderApi* session;
+
+		// The product service used to update product.
+		ProductService* productService;
+
+		// The init count down latch.
+		CountDownLatch<> initLatch;
+
+		// The start up count down latch.
+		CountDownLatch<> startLatch;
+
+		// Flag if stop is called or in process.
+		std::atomic<bool> stopFlag;
+
+		// The request ID value keeper.
+		int requestId;
+
+		// The exchange ID used to fill in the order.
+		std::string exchangeId;
+
+		// The investor ID to insert into order field.
+		std::string investorId;
+
+		// The trading date as from the session.
+		std::string tradingDate;
+
+		// The map where key is the instrument ID and value is the product message.
+		std::unordered_map<std::int64_t, ProductMessage> symbolMap;
+	};
+}
+
+
+
+#endif /* LIB_FEMAS_SRC_FEMASPRODUCTDOWNLOADSESSION_HPP_ */
